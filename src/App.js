99f9748924a79651c7cfa99ebac2ff42d1f34d68
iykeslim <iykeslim01@gmail.com>
2022-05-27 13:39:53 +0100

    filter logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Home from './components/Home.js';
+import {Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar.js';
+import Products from './components/Products.js';
+
 
 function App() {
   return (
    <>
     <Navbar/>
-    <Home/>
+    <Routes>
+      <Route exact path="/" element={<Home />}/>
+      <Route exact path="/products" element={<Products />}/>
+    </Routes>
    </>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,19 @@
 import React from 'react'
+import Products from './Products.js'
 
 export default function Home() {
   return (
-    <div>
-        <div className="card bg-dark text-white">
-  <img src="/assets/microphone.jpg" className="card-img" alt="..." />
-  <div className="card-img-overlay">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p className="card-text">Last updated 3 mins ago</p>
-  </div>
+    <div className='hero'>
+  <div className="card bg-dark text-white border-0">
+  <img src="/assets/microphone.jpg" className="card-img" alt="Background" height="550px" />
+  <div className="card-img-overlay d-flex flex-column justify-content-center">
+    <div className="container">
+    <h5 className="card-title display-3 fw-bolder mb-0">BEST MARKET LATESTS</h5>
+    <p className="card-text lead fs-2">VIEW OUR STOCK</p>
+    </div>
 </div>
+  </div>
+<Products />
     </div>
   )
 }
