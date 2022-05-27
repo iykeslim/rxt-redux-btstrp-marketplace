aec7ef49279dc85ce3ccae7288796c0ad205a9cb
iykeslim <iykeslim01@gmail.com>
2022-05-27 13:43:14 +0100

    UI changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
     <div>
         <nav className="navbar navbar-expand-lg bg-dark py-3 shadow-sm">
   <div className="container">
-    <NavLink className="navbar-brand fw-bold fs-4" to="#">IYKESLIM</NavLink>
+    <NavLink className="navbar-brand fw-bold fs-4" to="/">IYKESLIM</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
